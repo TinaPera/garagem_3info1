52da9de797fef2dee693cd7b2e2917818106a7aa
TinaPera <valentina.pera2007@gmail.com>
2024-07-12 08:22:50 -0300

    adição do campo n:n acessório no veículo

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -2,10 +2,14 @@ from django.db import models
 
 from .cor import Cor
 from .modelo import Modelo
-
+from .acessorio import Acessorio
 
 class Veiculo (models.Model):
     ano = models.IntegerField(default=0, null=True, blank=True)
     cor = models.ForeignKey(Cor, on_delete=models.RESTRICT)
     modelo = models.ForeignKey(Modelo, on_delete=models.RESTRICT)
     preco = models.DecimalField(decimal_places=2, max_digits=10, default=0, null=True, blank=True)
+    acessorio = models.ManyToManyField(Acessorio)
+
+    def __str__(self):
+        return f'{self.modelo} - {self.ano} - {self.cor}'
